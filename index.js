f250739c647fa90c38f7b196129e205b640ed47e
Adam Reis <adam@reis.nz>
2019-02-25 10:36:29 +1300

    Pass request and access token to callback

--- index.js
+++ index.js
@@ -3,52 +3,58 @@
 /**
  * Dependencies
  */
-const util = require('util');
-const Strategy = require('passport-strategy');
+const Strategy = require('@passport-next/passport-strategy');
 
 /**
- * Define refresh token strategy
+ * Refresh strategy
  */
-function RefreshStrategy(options, verify) {
+class RefreshStrategy extends Strategy {
 
-  //Parameter juggling
-  if (typeof options === 'function') {
-    verify = options;
-    options = {};
+  /**
+   * Constructor
+   */
+  constructor(options, callback) {
+
+    //Call parent constructor
+    super();
+
+    //Parameter juggling
+    if (typeof options === 'function') {
+      callback = options;
+      options = {};
+    }
+
+    //Setup strategy
+    this.name = 'refresh';
+    this.callback = callback;
   }
 
-  //Setup strategy
-  Strategy.call(this);
-  this.name = 'refresh';
-  this._verify = verify;
+  /**
+   * Authenticate request based on refresh token in http only cookie
+   */
+  authenticate(req) {
+
+    //Get refresh token hash from cookies
+    const hash = req.cookies.refreshToken;
+
+    //Get access token from headers
+    const auth = req.header('Authorization');
+    const parts = auth ? auth.split('Bearer ') : [];
+    const accessToken = parts[1] || null;
+
+    //Call callback
+    this.callback(req, hash, accessToken, (error, user, info) => {
+      if (error) {
+        return this.error(error);
+      }
+      if (!user) {
+        info = info || {};
+        return this.fail('invalid_token', info);
+      }
+      this.success(user, info);
+    }, req);
+  }
 }
-
-/**
- * Inherit from `passport.Strategy`.
- */
-util.inherits(RefreshStrategy, Strategy);
-
-/**
- * Authenticate request based on refresh token in http only cookie
- */
-RefreshStrategy.prototype.authenticate = function(req) {
-
-  //Get refresh token from cookies
-  const refreshToken =
-    req.cookies.refresh_token || req.cookies.refreshToken || null;
-
-  //Call verify handler
-  this._verify(refreshToken, (error, user, info) => {
-    if (error) {
-      return this.error(error);
-    }
-    if (!user) {
-      info = info || {};
-      return this.fail('invalid_token', info);
-    }
-    this.success(user, info);
-  }, req);
-};
 
 /**
  * Expose strategy
